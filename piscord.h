836ee692a7e0774b5acaf61b4b9ca586073033bd
Bernardo de Oliveira Bruning <bernardo.bruning@gmail.com>
2026-02-22 16:37:40 -0300

    docs: move TODO list to piscord.h header

--- piscord.h
+++ piscord.h
@@ -36,6 +36,12 @@
  *   This software is in the public domain. Where that dedication is not recognized, 
  *   you are granted a perpetual, irrevocable license to copy, distribute, 
  *   and modify this file as you see fit.
+ *
+ * TODO:
+ *   - [ ] Implement `mbedtls` backend for embedded systems.
+ *   - [ ] Implement `jsmn` or `js0n` backend for minimal JSON parsing.
+ *   - [ ] Add support for Discord Webhooks.
+ *   - [ ] Add support for Discord Gateway (WebSockets).
  */
 
 #ifndef PISCORD_H
